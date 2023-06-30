42e414efd565aa68350461a431129bf17172baa8
KAN-85 <ak-9000@yandex.ru>
2023-06-30 22:37:21 +0400

    Решение 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Николай", "Иванов", (byte) 26);
+        userService.saveUser("Александр", "Петров", (byte) 65);
+        userService.saveUser("Владимир", "Сидоров", (byte) 45);
+        userService.saveUser("Алексей", "Кузнецов", (byte) 32);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
+
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pp113","root", "root");
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+            return connection;
+    }
 }
+
+
+ /*public static Connection getConnection() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pp113",
+                "root", "root")) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println(1);
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+}*/
